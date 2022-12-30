71a55606c908263fae5883c7f6fe06a745f0a30a
yyy <2484218565@qq.com>
2022-12-30 17:13:11 +0800

    first commit

--- src/main/java/com/example/demo/wx/common/util/R.java
+++ src/main/java/com/example/demo/wx/common/util/R.java
@@ -1,2 +1,47 @@
-package com.example.demo.wx.common.util;public class R {
+package com.example.demo.wx.common.util;
+
+import org.apache.http.HttpStatus;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class R extends HashMap<String,Object> {
+//    定义R类的构造器
+    public R(){
+        put("code", HttpStatus.SC_OK);//200状态码
+        put("msg","success");
+    }
+//    error方法
+    public static R error(){
+        return error(HttpStatus.SC_INTERNAL_SERVER_ERROR,"未知异常");
+    }
+    public static R error(String msg){
+        return error(HttpStatus.SC_INTERNAL_SERVER_ERROR,msg);
+    }
+
+    private static R error(int code, String msg) {
+        R r=new R();
+        r.put("code",code);
+        r.put("msg",msg);
+        return r;
+    }
+    //ok方法
+    public static R ok(String msg){
+        R r=new R();
+        r.put("msg",msg);
+        return r;
+    }
+    public static R ok(Map<String,Object>map){
+        R r=new R();
+        r.putAll(map);
+        return r;
+    }
+    public static R ok(){
+        return new R();
+    }
+    public  R put(String key,Object value){
+        super.put(key,value);
+        return this;
+    }
+
 }
--- src/main/java/com/example/demo/wx/exception/AnsException.java
+++ src/main/java/com/example/demo/wx/exception/AnsException.java
@@ -1,2 +1,25 @@
-package com.example.demo.wx.exception;public class AnsException {
+package com.example.demo.wx.exception;
+
+import lombok.Data;
+
+@Data
+public class AnsException extends RuntimeException{
+//    类中包括状态码和消息
+    private String msg="";
+    private int code=500;
+    public AnsException(String msg){
+        super(msg);
+        this.msg=msg;
+    }
+//    重载的构造器
+    public AnsException(String msg,Throwable e){
+        super(msg,e);
+        this.msg=msg;
+    }
+    public AnsException(String msg,int code){
+        super(msg);
+        this.msg=msg;
+        this.code=code;
+
+    }
 }
